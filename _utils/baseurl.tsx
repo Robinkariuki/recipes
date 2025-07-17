9699313c073ea8be2da9f0f0a6c9308a68ff83d5
robin <robinkaris7@gmail.com>
2025-07-17 18:29:09 +0300

    made changes

--- _utils/baseurl.tsx
+++ _utils/baseurl.tsx
@@ -1,6 +1,4 @@
 export function getBaseUrl() {
-  if (typeof window !== "undefined") return ""; // browser should use relative path
-  return process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000"; // dev fallback
+  if (typeof window !== "undefined") return ""; // browser uses relative
+  return process.env.BASE_URL || "http://localhost:3000";
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Recipe } from "@/_components/Recipes";
 import { getBaseUrl } from "@/_utils/baseurl";
 
 export default async function Home() {
-  const res = await fetch(`${getBaseUrl()}/recipes.json`, {
-    cache: "no-store",
-  });
+const res = await fetch(`${getBaseUrl()}/recipes.json`, {
+  cache: "no-store",
+});
   const recipes: Recipe[] = await res.json();
 
   return <Recipes recipes={recipes} />;
