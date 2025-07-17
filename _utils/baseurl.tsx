21b84a9a7104e747bc849514b3735db3bb33d762
robin <robinkaris7@gmail.com>
2025-07-17 18:41:12 +0300

    made chages

--- _utils/baseurl.tsx
+++ _utils/baseurl.tsx
@@ -1,4 +1,3 @@
 export function getBaseUrl() {
-  if (typeof window !== "undefined") return ""; // browser uses relative
-  return process.env.BASE_URL || "http://localhost:3000";
+  return process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 }
